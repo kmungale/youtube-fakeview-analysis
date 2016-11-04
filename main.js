ec2a164e154f466a08e6f235128d69d5869b4559
macbook_user <macbook_user@CS-M100.local>
2016-11-04 00:25:15 -0500

    Adding node JS file

---
+++ main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by macbook_user on 11/2/16.
+ */
+
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+  res.send('Hello Seattle\n');
+});
+
+app.get('/sample', function(req, res){
+    console.log("hurray");
+    res.send({
+        "name": "kaustubh"
+    });
+    console.log(req.headers)
+});
+
+app.listen(3001);
+console.log('Listening on port 3001...');
