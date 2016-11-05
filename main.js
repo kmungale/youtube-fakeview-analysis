3446f611fdfdb5b358507ac45f2ce6268d459ed9
macbook_user <macbook_user@wireless-nat-inside.uiowa.edu>
2016-11-04 19:31:19 -0500

    adding new dependencies in nodes js

--- main.js
+++ main.js
@@ -3,16 +3,22 @@
  */
 
 var express = require('express');
+var request = require('request');
 var app = express();
 
 //added process.env.PORT because Hreoku dynamically allocates PORT
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res) {
+    request('https://freegeoip.net/json/?callback=', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(response.body); // Show the HTML for the Google homepage.
+      }
+    });
+    console.log(req.headers);
     res.sendfile("html/index.html")
 });
 
-app.get('/sample', function(req, res){
-    console.log("hurray");
+app.post('/trackMouseMotion', function(req, res){
     res.send({
         "name": "kaustubh"
     });
