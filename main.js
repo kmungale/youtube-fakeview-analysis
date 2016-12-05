4c744630acc4b09e1c294ce37f55ffd397aa2831
kmungale <macbook_user@CS-M100.local>
2016-12-04 20:38:58 -0600

    removing flag from reputation

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ var userData = mongoose.model('userData', userSchema);
 app.set('port', (process.env.PORT || 3001));
 app.get('/', function(req, res) {
     var search_ip_by_location = 'http://freegeoip.net/json/' + (req.headers['x-forwarded-for'] || '63.152.57.234');
-    var check_ip_reputation =  'http://check.getipintel.net/check.php?ip=' + (req.headers['x-forwarded-for'] || '63.152.57.234') + '&contact=kaustubhmungale@yahoo.com&format=json&flags=m';
+    var check_ip_reputation =  'http://check.getipintel.net/check.php?ip=' + (req.headers['x-forwarded-for'] || '63.152.57.234') + '&contact=kaustubhmungale@yahoo.com&format=json';
     var location;
     user.userAgent = req.headers['user-agent'];
     user.referer = req.headers['referer'];
